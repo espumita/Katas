83ec8a813b782dd3de56cac0f8f68766837116d6
espumita <davidfalconnaranjo@gmail.com>
2024-08-03 11:41:33 +0100

    Add Roman Numerals Kata

---
+++ RomanNumerals/src/RomanNumerals/RomanNumerals.cs
@@ -0,0 +1,30 @@
+﻿namespace RomanNumerals;
+
+public class RomanNumerals {
+
+    private static readonly Dictionary<int, string> FormatedNumbers = new Dictionary<int, string>() {
+        { 1, "I" },
+        { 4, "IV" },
+        { 5, "V"},
+        { 9, "IX"},
+        { 10, "X"},
+        { 40, "XL"},
+        { 50, "L"},
+        { 90, "XC"},
+        { 100, "C"}
+    };
+
+    public string Execute(int number) => number switch {
+        _ when FormatedNumbers.ContainsKey(number) => FormatedNumbers[number],
+        > 100 => Add(100, number),
+        > 90 => Add(90, number),
+        > 50 => Add(50, number),
+        > 40 => Add(40, number),
+        > 10 => Add(10, number),
+        _ => Execute(number - 1) + FormatedNumbers[1]
+    };
+
+    private string Add(int value, int number) {
+        return FormatedNumbers[value] + Execute(number - value);
+    }
+}
---
+++ RomanNumerals/test/RomanNumerals.Tests/RomanNumeralsTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+
+namespace RomanNumerals.Tests;
+
+public class RomanNumeralsTests {
+
+    [TestCase(1, "I")]
+    [TestCase(2, "II")]
+    [TestCase(3, "III")]
+    [TestCase(4, "IV")]
+    [TestCase(5, "V")]
+    [TestCase(6, "VI")]
+    [TestCase(7, "VII")]
+    [TestCase(8, "VIII")]
+    [TestCase(9, "IX")]
+    [TestCase(10, "X")]
+    [TestCase(11, "XI")]
+    [TestCase(12, "XII")]
+    [TestCase(13, "XIII")]
+    [TestCase(14, "XIV")]
+    [TestCase(18, "XVIII")]
+    [TestCase(19, "XIX")]
+    [TestCase(20, "XX")]
+    [TestCase(39, "XXXIX")]
+    [TestCase(40, "XL")]
+    [TestCase(41, "XLI")]
+    [TestCase(49, "XLIX")]
+    [TestCase(50, "L")]
+    [TestCase(51, "LI")]
+    [TestCase(89, "LXXXIX")]
+    [TestCase(90, "XC")]
+    [TestCase(99, "XCIX")]
+    [TestCase(100, "C")]
+    [TestCase(399, "CCCXCIX")]
+    public void return_ronam_numeral(int number, string romanNumeral) {
+        var romanNumerals = new RomanNumerals();
+
+        var output = romanNumerals.Execute(number);
+
+        output.Should().Be(romanNumeral);
+    }
+
+}
